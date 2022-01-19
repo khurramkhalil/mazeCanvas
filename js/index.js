f44e60e447b78897ba61064abe3b12675454b6e2
Khurram Khalil <kkhalil.pg@smme.edu.pk>
2022-01-19 03:04:02 -0800

    Maze application with three arrays holding information for directions in working order.

--- js/index.js
+++ js/index.js
@@ -22,29 +22,88 @@ Runner.run(Runner.create(), engine)
 
 // Walls
 const walls = [
-  Bodies.rectangle(width/2, 0, width, 40, {isStatic: true}),
-  Bodies.rectangle(width/2, height, width, 40, {isStatic: true}),
-  Bodies.rectangle(0, height/2, 40, height, {isStatic: true}),
-  Bodies.rectangle(width, height/2, 40, height, {isStatic: true})
+  Bodies.rectangle(width / 2, 0, width, 40, {isStatic: true}),
+  Bodies.rectangle(width / 2, height, width, 40, {isStatic: true}),
+  Bodies.rectangle(0, height / 2, 40, height, {isStatic: true}),
+  Bodies.rectangle(width, height / 2, 40, height, {isStatic: true})
 ]
 
 World.add(world, walls)
 
 // Maze generation
 
+
+const shuffle = (arr) => {
+  let counter = arr.length;
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+    counter--
+
+    const temp = arr[counter];
+    arr[counter] = arr[index]
+    arr[index] = temp;
+  }
+
+  return arr;
+}
+
 // Elegant way for initializing 2D arrays with default values, instead we can use for loops, but they are clunky
 const grid = Array(cells).fill(null).map(() => Array(cells).fill(false))
 
-const verticals = Array(cells).fill(null).map(() => Array(cells-1).fill(false))
-const horizontals = Array(cells-1).fill(null).map(() => Array(cells).fill(false))
-console.log(grid, verticals, horizontals)
+const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false))
+const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(false))
 
 
+const startRow = Math.floor(Math.random() * cells)
+const startColumn = Math.floor(Math.random() * cells)
 
+const stepThroughCell = (row, column) => {
+  //   If awe visited current cel already
+  if (grid[row][column] === true) {
+    return;
+  }
+  //  Mark the cell visited
+  grid[row][column] = true;
 
+  // Assemble randomly-ordered list of neighbors
+  const neighbors = shuffle([
+    [row - 1, column, 'up'],
+    [row, column + 1, 'right'],
+    [row + 1, column, 'down'],
+    [row, column - 1, 'left']
+  ]);
 
+  // For each neighbor...
+  for (let neighbor of neighbors) {
+    const [nextRow, nextColumn, direction] = neighbor
+    // See if that neighbor is out of bound
+    if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+      continue;
+    }
+    // if we have visited that neighbor, continue to next neighbor
+    if (grid[nextRow][nextColumn]) {
+      continue;
+    }
+    // Remove wall from either horizontal or vertical
+    if (direction === 'left') {
+      verticals[row][column - 1] = true
+    }
+    else if (direction === 'right') {
+      verticals[row][column] = true
+    }
+    else if(direction === 'up'){
+      horizontals[row -1][column] = true;
+    }
+    else if(direction === 'down'){
+      horizontals[row][column] = true
+    }
+  }
 
+}
 
+stepThroughCell(startRow, startColumn)
+// console.log(grid)
 
 
 
